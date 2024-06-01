aa8f27a0f7769bb417d0a4253329d08d354ce20d
Arthur <arthur.perronnet@gmail.com>
2024-06-01 19:35:56 +0200

    Fixed a bug where game with age restriction crashed the code

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const client = new TwitterApi({
     accessSecret: config.accessSecret,
 });
 
+function sleep(ms) {
+
+    // Permet de mettre en pose le script pendant ms millisecondes
+    return new Promise(resolve => setTimeout(resolve, ms));
+
+}
+
 function getGMTOffset() {
 
     // Renvoie l'offset en minutes
@@ -110,82 +117,101 @@ setInterval(() => {
                     console.log(`Je vérifie le jeu en promo n°${i+1}`);
                     const url = freeGamesList[i];
                     const appId = url.replace('https://store.steampowered.com/app/', "").split("/")[0];
-                    await page.goto(url);
+                    await page.goto(url, { waitUntil: 'load' });
+                    try {
+                        // Si le jeu a une limite d'âge on n'a pas directement accès aux infos...
+                        // On donne donc un âge valide pour accèder à la page
+                        console.log("Bypass d'une possible limite d'age");
+                        // C'est ma vrai date de naissance si jamais
+                        await page.select('#ageDay', '24');
+                        await page.select('#ageMonth', 'February');
+                        await page.select('#ageYear', '2005');
+                        await page.click('.agegate_btn_ctn');
+                        console.log("Limite d'age bypassée avec succès");
+
+                    } catch (err) {
+                        console.log("Pas de limite d'age");
+                    }
+                    await sleep(2500);
                     const title = await page.$eval('.apphub_AppName', el => el.textContent);
-                    const desc = await page.$eval('.game_purchase_discount_quantity ', el => el.textContent);
-                    const priceofgame = await page.evaluate(() => {
-
-                        let data_price = [];
-                        let elements = document.querySelectorAll('.discount_original_price');
-                        for(let element of elements) {
-
-                            data_price.push(element.textContent);
-
-                        }
-                        return data_price;
-
-                    });
-                    // La date est pas en clair, donc on fait encore un peu de magie noire
-                    const date = desc.split("\t")[0].replace("Free to keep when you get it before ", "").replaceAll(".", "");
-                    // On vérifie si on a déjà scrap cette promo
-                    if(!data.games.includes(title)) {
-
-                        console.log(`Nouveau jeu gratuit: ${title}`);
-                        data.games.push(title);
-                        var gameData = {
-
-                            "title": title,
-                            "date": Date.parse(`${parseInt(date.split(" @ ")[0].split(" ")[0])+1} ${date.split(" @ ")[0].split(" ")[1]} ${convertTo24Format(date.split(" @ ")[1])} ${new Date().getFullYear()}`),
-                            // On prend le premier élément car les prix des dlc sont aussi dans la liste
-                            "price": priceofgame[0],
-                            "img": `https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/${appId}/header.jpg`,
-                            "link": url
-
-                        }
-                        data.games_data.push(gameData);
-                        console.log(`J'ai ajouté ${title}`);
-
-
-
-                        // Création de l'image attachée au tweet via canvas
-                        const canvas = createCanvas(600, 407);
-                        const ctx = canvas.getContext('2d');
-                        registerFont('./assets/Montserrat-ExtraBold.ttf', { family: 'Mont' });
-                        ctx.font = '50px Mont';
-                        ctx.fillStyle = "#ffffff";
-                        ctx.strokeStyle = "#000000";
-                        ctx.lineWidth = 4;
-                        loadImage('./assets/wallpaper.png').then((image) => {
-                            ctx.drawImage(image, 0, 0);
-                            const promise = loadImage(gameData.img);
-                            promise.then(async cover => {
-                                ctx.drawImage(cover, 70, 70);
-                                var textToPrint = `${gameData.price} ➜ 0,00€`;
-                                var txtSize = ctx.measureText(textToPrint).width;
-                                var txtXPos = 300-txtSize/2;
-                                ctx.strokeText(textToPrint, txtXPos, 330);
-                                ctx.fillText(textToPrint, txtXPos, 330);
-                                const buffer = canvas.toBuffer("image/png");
-
-
-
-                                // Envoie du tweet
-                                // Ces variables servent pour l'écriture de la date
-                                var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
-                                var DDay = new Date(gameData.date);
-                                const mediaIds = await Promise.all([
-                                    client.v1.uploadMedia(buffer, { mimeType: 'png' })
-                                ]);
-                                client.v2.tweet({
-                                    text: `The game "${gameData.title}" is free on Steam until the ${DDay.getDate()} ${months[DDay.getMonth()]} at ${addZeroIfNeeded(DDay.getHours())}:${addZeroIfNeeded(DDay.getMinutes())} ${getGMTOffset().split(":")[0]}.\nGet the game here: ${gameData.link}\n\n#FreeGame #SteamGame`,
-                                    media: { media_ids: mediaIds }
-                                });
-
-
-
+                    try {
+                        const desc = await page.$eval('.game_purchase_discount_quantity ', el => el.textContent);
+                        const priceofgame = await page.evaluate(() => {
+    
+                            let data_price = [];
+                            let elements = document.querySelectorAll('.discount_original_price');
+                            for(let element of elements) {
+    
+                                data_price.push(element.textContent);
+    
+                            }
+                            return data_price;
+    
+                        });
+                        // La date est pas en clair, donc on fait encore un peu de magie noire
+                        const date = desc.split("\t")[0].replace("Free to keep when you get it before ", "").replaceAll(".", "");
+                        // On vérifie si on a déjà scrap cette promo
+                        if(!data.games.includes(title)) {
+    
+                            console.log(`Nouveau jeu gratuit: ${title}`);
+                            data.games.push(title);
+                            var gameData = {
+    
+                                "title": title,
+                                "date": Date.parse(`${parseInt(date.split(" @ ")[0].split(" ")[0])+1} ${date.split(" @ ")[0].split(" ")[1]} ${convertTo24Format(date.split(" @ ")[1])} ${new Date().getFullYear()}`),
+                                // On prend le premier élément car les prix des dlc sont aussi dans la liste
+                                "price": priceofgame[0],
+                                "img": `https://shared.cloudflare.steamstatic.com/store_item_assets/steam/apps/${appId}/header.jpg`,
+                                "link": url
+    
+                            }
+                            data.games_data.push(gameData);
+                            console.log(`J'ai ajouté ${title}`);
+    
+    
+    
+                            // Création de l'image attachée au tweet via canvas
+                            const canvas = createCanvas(600, 407);
+                            const ctx = canvas.getContext('2d');
+                            registerFont('./assets/Montserrat-ExtraBold.ttf', { family: 'Mont' });
+                            ctx.font = '50px Mont';
+                            ctx.fillStyle = "#ffffff";
+                            ctx.strokeStyle = "#000000";
+                            ctx.lineWidth = 4;
+                            loadImage('./assets/wallpaper.png').then((image) => {
+                                ctx.drawImage(image, 0, 0);
+                                const promise = loadImage(gameData.img);
+                                promise.then(async cover => {
+                                    ctx.drawImage(cover, 70, 70);
+                                    var textToPrint = `${gameData.price} ➜ 0,00€`;
+                                    var txtSize = ctx.measureText(textToPrint).width;
+                                    var txtXPos = 300-txtSize/2;
+                                    ctx.strokeText(textToPrint, txtXPos, 330);
+                                    ctx.fillText(textToPrint, txtXPos, 330);
+                                    const buffer = canvas.toBuffer("image/png");
+    
+    
+    
+                                    // Envoie du tweet
+                                    // Ces variables servent pour l'écriture de la date
+                                    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+                                    var DDay = new Date(gameData.date);
+                                    const mediaIds = await Promise.all([
+                                        client.v1.uploadMedia(buffer, { mimeType: 'png' })
+                                    ]);
+                                    client.v2.tweet({
+                                        text: `The game "${gameData.title}" is free on Steam until the ${DDay.getDate()} ${months[DDay.getMonth()]} at ${addZeroIfNeeded(DDay.getHours())}:${addZeroIfNeeded(DDay.getMinutes())} ${getGMTOffset().split(":")[0]}.\nGet the game here: ${gameData.link}\n\n#FreeGame #SteamGame`,
+                                        media: { media_ids: mediaIds }
+                                    });
+    
+    
+    
+                                })
                             })
-                        })
-
+    
+                        }
+                    } catch (err) {
+                        console.log("C'est un dlc, on annule");
                     }
                 }
 
